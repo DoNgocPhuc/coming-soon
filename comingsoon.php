02800234b4f7eb771c4833babfe6611b21e8e767
Đỗ Ngọc Phúc <35281868+DoNgocPhuc@users.noreply.github.com>
2022-03-24 17:48:39 +0700

    Update comingsoon.php

--- comingsoon.php
+++ comingsoon.php
@@ -3,7 +3,7 @@
 Plugin Name: Coming Soon
 Plugin URI: http://wordpress.org/plugins/hello-dolly/
 Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
-Author: Wiloke
+Author: Đỗ Ngọc Phúc
 Version: 1.0
 Author URI: http://wiloke.com
 */
